52a82951bd364379d33d174a3a98ee2dfb0a032f
burning846 <sbn1998@pku.edu.cn>
2019-08-18 02:32:32 +0800

    20190818 init

--- cpp/TwentyFourPointsSlover.cpp
+++ cpp/TwentyFourPointsSlover.cpp
@@ -119,11 +119,6 @@ public:
 
 int main()
 {
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(5);
-	v.push_back(5);
-	v.push_back(5);
-	TwentyFourPointsSolver().solve(v);
+	TwentyFourPointsSolver().solve(vector<int>{1,5,5,5});
 	cin.get();
 }
